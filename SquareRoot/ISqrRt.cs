789adb1665ab437387526e42657f9fc9335eb57b
RubyBe <benavente@outlook.com>
2016-11-29 11:31:06 -0800

    add unit test file and adjust code to fit

--- SquareRoot/ISqrRt.cs
+++ SquareRoot/ISqrRt.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SquareRoot
 {
-    interface ISqrRt
+    public interface ISqrRt
     {
         double GetSquareRoot(double number);
     }
--- SquareRootTest/ComparisonTest.cs
+++ SquareRootTest/ComparisonTest.cs
@@ -1,58 +1,58 @@
-﻿//using System;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using System.Collections.Generic;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using SquareRoot;
 
-//namespace SquareRootTest
-//{
-//    [TestClass]
-//    public class ComparisonTest
-//    {
-//        [TestMethod]
-//        public void ValidateAndCompareCalculationsWithinErrorLimits()
-//        {
-//            // Arrange
-//            double error = 0.0001;
-//            double resultHeron;
-//            double resultStandard;
-//            int resultTrue = 0;
-//            List<bool> resultList = new List<bool>();
-//            HeronCalculator heronCalculator = new HeronCalculator(error);
-//            StandardCalculator standardCalculator = new StandardCalculator(error);
-//            // Create random sample data to use for testing square root methods
-//            Random r = new Random();
-//            List<int> listNumbers = new List<int>();
-//            for (int i = 1; i < 10001; i++)
-//            {
-//                listNumbers.Add(r.Next(1, 100000));
-//            }
-//            // Act
 
-//            // Insert
-//        }
-//    }
-//}
+namespace SquareRootTest
+{
+    [TestClass]
+    public class ComparisonTest
+    {
+        [TestMethod]
+        public void ValidateAndCompareCalculationsWithinErrorLimits(ISqrRt heronCalculator, ISqrRt standardCalculator, double error)
+        {
+            // Arrange
+            double resultHeron;
+            double resultStandard;
+            bool resultComparison = false;
+            // Create random sample data to use for testing square root methods
+            Random r = new Random();
+            List<int> listNumbers = new List<int>();
+            for (int i = 1; i < 10001; i++)
+            {
+                listNumbers.Add(r.Next(1, 100000));
+            }
+            // Act
+            // Run both square root methods on each of the random sample data numbers
+            foreach (var num in listNumbers)
+            {
+                resultHeron = heronCalculator.GetSquareRoot(num);
+                resultStandard = standardCalculator.GetSquareRoot(num);
+                resultComparison = CompareRoots(resultHeron, resultStandard, error);
+            }
+            // Assert
+            Assert.IsTrue(resultComparison);
+        }
+        public bool CompareRoots(double heron, double standard, double error)
+        {        
+            int resultTrue = 0;
+            List<bool> resultList = new List<bool>();
+            List<double> errorList = new List<double>();
+            if (Math.Abs(heron - standard) < error)
+            {
+                resultList.Add(true);
+                errorList.Add(Math.Abs(heron - standard));
+                resultTrue++;
+                return true;
+            }
+            else
+            {
+                resultList.Add(false);
+                return false;
+                //throw new Exception("The difference between the two methods is not within the accepted error rate.");
+            }
+        }
+    }
+}
 
-//// ------------------------------------------------------------
-//private static double ValidateHeron()
-//{
-//    // A function to validate the accuracy of the Heron calculation vs. the internal Math.Sqrt function
-
-//    // Run both square root methods on each of the random sample data numbers
-//    foreach (var num in listNumbers)
-//    {
-//        resultHeron = heronCalculator.GetHeronRoot(num);
-//        resultStandard = standardCalculator.GetStandardRoot(num);
-//        if (Math.Abs(resultHeron - resultStandard) < error)
-//        {
-//            resultList.Add(true);
-//            resultTrue++;
-//        }
-//        else
-//        {
-//            resultList.Add(false);
-//            throw new Exception("The difference between the two methods is not within the accepted error rate.");
-//        }
-//    }
-
-//    return resultTrue;
-//}
